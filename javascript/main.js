469641308fce0ede4bae6bb422f292987f48914a
bucketsoflava <brandonlau1055@gmail.com>
2017-01-31 21:55:47 +0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -3,11 +3,14 @@
 
 $(document).ready(function () {
     "use strict";
+    $("#number").hide();
     $("#container1").hide();
     $("#container2").hide();
     $("#translucentBG").hide();
     $("#setKeys").hide();
     $("#howToPlay").hide();
+    $("#scoreboard").hide();
+    $("#centerText").hide();
     $("#play").click(function () {
         $("#menu").hide();
         $("#howToPlay").show();
